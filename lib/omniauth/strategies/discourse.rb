91f4aa6c41940030fc2b6850b2448559b08e0e8a
Ioannis Tsagkatakis <jtsagata@gmail.com>
2020-08-25 23:22:21 +0300

    Update vesrion, use addressable

--- lib/omniauth/strategies/discourse.rb
+++ lib/omniauth/strategies/discourse.rb
@@ -5,27 +5,27 @@ module OmniAuth
   module Strategies
     class Discourse
       include OmniAuth::Strategy
-      
+
       args [:sso_secret, :sso_url]
       option :sso_secret, nil
       option :sso_url, nil
-      
+
       attr_reader :user_info
-      
+
       def request_phase
         sso = SSO.new(options.sso_secret, options.sso_url, callback_url)
         session[:sso_nonce] = sso.nonce
-          
+
         redirect sso.request_url
       end
-      
+
       def callback_phase
         sso = SSO.new(options.sso_secret, options.sso_url, callback_url, session[:sso_nonce])
         sso.parse(request.params)
         raise OmniAuth::NoSessionError, "Username or password are not valid" if sso.status == "error"
 
         @user_info = sso.user_info
-        
+
         super
       rescue OmniAuth::NoSessionError => e
         fail!(:invalid_credentials, e)
@@ -39,14 +39,14 @@ module OmniAuth
         {
           "name" => user_info[:name],
           "email" => user_info[:email],
-          "nickname" => user_info[:username]
+          "nickname" => user_info[:username],
         }
       end
 
       extra do
         {
           "admin" => user_info[:admin] == "true",
-          "moderator" => user_info[:moderator] == "true"
+          "moderator" => user_info[:moderator] == "true",
         }
       end
     end
--- lib/omniauth/strategies/discourse/sso.rb
+++ lib/omniauth/strategies/discourse/sso.rb
@@ -5,7 +5,7 @@ module OmniAuth
     class Discourse
       class SSO
         attr_accessor :nonce, :user_info, :status, :message
-        
+
         def initialize(sso_secret, sso_url, return_url, nonce = nil)
           @sso_secret, @sso_url, @return_url = sso_secret, sso_url, return_url
           @nonce = nonce ? nonce : generate_nonce!
@@ -16,16 +16,16 @@ module OmniAuth
         end
 
         def request_url
-          "#{ @sso_url }?sso=#{ url_encoded_payload }&sig=#{ hex_signature }"
+          "#{@sso_url}?sso=#{url_encoded_payload}&sig=#{hex_signature}"
         end
 
         def parse(params)
-          #params should be something that looks like: {"sso": "xxxxxx", "sig": "yyyyyy"} 
-          if get_hmac_hex_string(params["sso"]) == params["sig"] 
+          #params should be something that looks like: {"sso": "xxxxxx", "sig": "yyyyyy"}
+          if get_hmac_hex_string(params["sso"]) == params["sig"]
             if base64? params["sso"]
               decoded_hash = Rack::Utils.parse_query(Base64.decode64(params["sso"]))
               decoded_hash.symbolize_keys!
-              if decoded_hash[:nonce] == @nonce   
+              if decoded_hash[:nonce] == @nonce
                 @status = "success"
                 decoded_hash.delete(:nonce)
                 @user_info = decoded_hash
@@ -36,50 +36,50 @@ module OmniAuth
                 @user_info = nil
                 @message = "SSO verification failed. Nonce mismatch."
                 return nil
-              end  
+              end
             else
               @status = "error"
               @user_info = nil
               @message = "The sso string is supposed to be encoded in Base64."
-              return nil       
+              return nil
             end
           else
             @status = "error"
             @user_info = nil
             @message = "HMAC mismatch. The message may have been tampered with."
-            return nil          
+            return nil
           end
         end
 
-        private 
-          def raw_payload
-            unless @nonce
-              raise "You must generate a nonce by calling generate_nonce! first."
-            else
-              "nonce=#{ @nonce }&return_sso_url=#{ @return_url }"
-            end
-          end
+        private
 
-          def base64_encoded_payload
-            Base64.encode64(raw_payload)
+        def raw_payload
+          unless @nonce
+            raise "You must generate a nonce by calling generate_nonce! first."
+          else
+            "nonce=#{@nonce}&return_sso_url=#{@return_url}"
           end
+        end
 
-          def url_encoded_payload
-            URI.escape(base64_encoded_payload)
-          end
+        def base64_encoded_payload
+          Base64.encode64(raw_payload)
+        end
 
-          def hex_signature
-            get_hmac_hex_string base64_encoded_payload
-          end
+        def url_encoded_payload
+          Addressable::URI.escape(base64_encoded_payload)
+        end
 
-          def get_hmac_hex_string payload
-            OpenSSL::HMAC.hexdigest("sha256", @sso_secret, payload)
-          end
+        def hex_signature
+          get_hmac_hex_string base64_encoded_payload
+        end
 
-          def base64? data
-            !(data =~ /[^a-zA-Z0-9=\r\n\/+]/m)
-          end
+        def get_hmac_hex_string(payload)
+          OpenSSL::HMAC.hexdigest("sha256", @sso_secret, payload)
+        end
 
+        def base64?(data)
+          !(data =~ /[^a-zA-Z0-9=\r\n\/+]/m)
+        end
       end
     end
   end
